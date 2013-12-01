2be2f9bf563fb2c3c5c2477c253674edffd168ff
amyskerry <amy.skerry@gmail.com>
2013-11-30 20:27:31 -0500

    Update question.py

--- question.py
+++ question.py
@@ -48,13 +48,13 @@ else:
         qnum=qnums[qindex-1]
         question=questions[int(qnum)-1]
         qname='jorie'
-        question="{}s's boyfriend bought her a necklace. she really liked it. {}s smiled"
+        mainquestion="{}s's boyfriend bought her a necklace. she really liked it. {} smiled"
         
         for ltr in question:
-        	if ltr == '%':
+        	if ltr == '{':
         		nameargs.append(qname)
         nameargs=['jorie', 'tina']
-        question=question.format(*nameargs)
+        question=mainquestion.format(*nameargs)
         qindex=str(qindex)
         qnumlist=qnums[0]   
         count=0
