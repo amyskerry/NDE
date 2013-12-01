179aaa2f3c96fc44759fb531b8f9b72de2df9a1a
amyskerry <amy.skerry@gmail.com>
2013-11-30 20:16:58 -0500

    Update question.py

--- question.py
+++ question.py
@@ -53,7 +53,7 @@ else:
         #for ltr in question:
         #	if ltr == '%':
         #		nameargs.append(qname)
-        question=question % (**nameargs)
+        question=question % (nameargs)
         qindex=str(qindex)
         qnumlist=qnums[0]   
         count=0
