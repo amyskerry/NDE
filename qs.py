49f6fb8019c2bbbee2b4f1f8a770b3db094c0acf
askerry <amy.skerry@gmail.com>
2013-12-01 14:54:01 -0500

    cleaning up

--- qs.py
+++ qs.py
@@ -1,3 +1,5 @@
 #questions=['question aaaa', 'question b', 'question cccc', 'quetsion d', 'question fff', 'question gggg']
 questions=['questiNAMEVARon aksjadlhglaksjghhhhhhhhhhNAMEVARhhhhhhhhhhhhhhhhslasdgjkasdhgalskjdhg klsajdhglkjashglkjashdglkjh lkjashdkljahsdlkg lkhasdgkjhaslkdgh lkjahsdfkahsdfkljahd jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj jjjjjjjjjjjjjjjjjjjjjjjjjj aaa', 'queNAMEVARstion b', 'NAMEVAR ksdhgaksdhg jkasdgkjadf NAMEVAR"s kashglkashglkasjgh kashdgklashdgkljh NAMEVARkshdgkdshg']
 names=['mina', 'jorie', 'amy']
+emolist=['agitated', 'amused', 'angry', 'annoyed', 'anxious', 'ashamed', 'contemptuous', 'despairing', 'disappointed', 'disgusted', 'dread', 'enthusiastic', 'grateful', 'guilty', 'hopeful', 'impressed', 'joyful', 'loneliness', 'pleased', 'proud', 'sad', 'scared']
+
--- question.py
+++ question.py
@@ -5,8 +5,9 @@ import cgitb, cgi, MySQLdb, ast
 import cPickle as p
 from random import randint, shuffle
 from ast import literal_eval
-from qs import questions, names
+from qs import questions, names, emolist
 from slist import subjects, keycodes
+import math
 
 myform=cgi.FieldStorage()
 cgitb.enable()
@@ -89,8 +90,6 @@ else:
                        		newsql='update demographics_tbl set '+newqvar+'="'+it+'" where rownum="'+formindex+'"'
                        		cursor.execute(newsql)
                		except: pass
-        
-        
         ### css setup
         print '''
         <head><title>Research Study</title>
@@ -110,12 +109,13 @@ else:
         <style type="text/css" media="all">@import "css/style.css";</style>
         <style type="text/css" media="all">
 	.radioLeft{
-    		text-align:left;
+    		text-align: center;
 		display:inline-block;	
 	}
-	label {
- 		 margin-left: 10px;
+	label { 		 
+		 margin-left: 15px;
 		 margin-right: 15px;
+
 	}
 	</style>
 	<script type="text/javascript">
@@ -132,105 +132,58 @@ else:
         
         </script>
         </head>
-        '''
+	'''
         ####end of setup
+	nextthing='question.py'
         if int(qindex)<len(questions):
-       		#print "main loop"
-       		print "<center><b>Question %s/%s:</b><br><br>" % (qindex, len(questions))
-       		print "<center>%s. <br><br>How does the character feel in this situation? <br> " % (question)
-       		print '''
-        	<div id="page_content" align="center">
-        	<form name="myform" action="question.py" method="submit" onSubmit="return validate(myform)">
-        	<div class="radioLeft" align="center">
-                <br><input type="radio" name="response" value="good"><label for="good">good</label>
-                <br><input type="radio" name="response" value="sad"><label for="sad">sad</label>
-                <br><input type="radio" name="response" value="amused"><label for="amused">amused</label>
-                <br><input type="radio" name="response" value="bad"><label for="bad">bad</label>
-                <br><input type="radio" name="response" value="angry"><label for="angry">angry</label>
-                <br><input type="radio" name="response" value="anxious"><label for="anxious">anxious</label>
-                <br><input type="radio" name="response" value="scared"><label for="scared">scared</label>
-                <br><input type="radio" name="response" value="lonely"><label for="lonely">lonely</label>
-                <br><input type="radio" name="response" value="loving"><label for="loving">loving</label>
-                </div>
-                <div class="radioLeft" align="center">
-                <br><input type="radio" name="response" value="good"><label for="good">good</label>
-                <br><input type="radio" name="response" value="sad"><label for="sad">sad</label>
-                <br><input type="radio" name="response" value="amused"><label for="amused">amused</label>
-                <br><input type="radio" name="response" value="bad"><label for="bad">bad</label>
-                <br><input type="radio" name="response" value="angry"><label for="angry">angry</label>
-                <br><input type="radio" name="response" value="anxious"><label for="anxious">anxious</label>
-                <br><input type="radio" name="response" value="scared"><label for="scared">scared</label>
-                <br><input type="radio" name="response" value="lonely"><label for="lonely">lonely</label>
-                <br><input type="radio" name="response" value="loving"><label for="loving">loving</label>
-                </div>
-                <div class="radioLeft" align="center">
-                <br><input type="radio" name="response" value="good"><label for="good">good</label>
-                <br><input type="radio" name="response" value="sad"><label for="sad">sad</label>
-                <br><input type="radio" name="response" value="amused"><label for="amused">amused</label>
-                <br><input type="radio" name="response" value="bad"><label for="bad">bad</label>
-                <br><input type="radio" name="response" value="angry"><label for="angry">angry</label>
-                <br><input type="radio" name="response" value="anxious"><label for="anxious">anxious</label>
-                <br><input type="radio" name="response" value="scared"><label for="scared">scared</label>
-                <br><input type="radio" name="response" value="lonely"><label for="lonely">lonely</label>
-                <br><input type="radio" name="response" value="loving"><label for="loving">loving</label>
-                </div>
-                <div class="radioLeft" align="center">
-                <br><input type="radio" name="response" value="good"><label for="good">good</label>
-                <br><input type="radio" name="response" value="sad"><label for="sad">sad</label>
-                <br><input type="radio" name="response" value="amused"><label for="amused">amused</label>
-                <br><input type="radio" name="response" value="bad"><label for="bad">bad</label>
-                <br><input type="radio" name="response" value="angry"><label for="angry">angry</label>
-                <br><input type="radio" name="response" value="anxious"><label for="anxious">anxious</label>
-                <br><input type="radio" name="response" value="scared"><label for="scared">scared</label>
-                <br><input type="radio" name="response" value="lonely"><label for="lonely">lonely</label>
-                <br><input type="radio" name="response" value="loving"><label for="loving">loving</label>
-                </div>
-
-		<br><br>
-		<p> If you feel that the situation is better described by a word not <br> provided above, please list alternative words here (optional). </p>
-        	<input type="text" name="otherword1" >
-        	<br><input type="text" name="otherword2">
-        	<br><br>
-        	<input type="hidden" name="subjid" value="'''+subjid+'''">
-        	<input type="hidden" name="keycode" value="'''+keycode+'''">
-        	<input type="hidden" name="qindex" value="'''+qindex+'''">
-        	<input type="hidden" name="qnums" value="'''+qnumlist+'''"> 
-        	<input type="hidden" name="rownum" value="'''+formindex+'''"> 	
-        	<input type="submit" value="Continue" /></center>
-        	<br><br><br><br>
-        	</form>
-        	</div>
-        	</body>
-        	</html>
-        	'''
-        else:
-	       	#print "final loop"
-		print "Question %s/%s:<br>" % (qindex, len(questions))
-       		print "%s. How does the target feel? " % (question)
-       		print '''
-        	<div id="page_content" align="center">
-        	<form name="myform" action="demographics.py" method="submit" onSubmit="return validate(myform)">
-        	<br><input type="radio" name="response" value="good">GOOD
-        	<br><input type="radio" name="response" value="bad">BAD
-        	<br>
-        	<input type="text" name="otherword1" >
-        	<br><input type="text" name="otherword2">
-        	<br>
-        	<p style="text-align:center">You have completed the primary task! We have a few quick follow up questions for you.</p>
-        	<input type="hidden" name="subjid" value="'''+subjid+'''">
-        	<input type="hidden" name="keycode" value="'''+keycode+'''">
-        	<input type="hidden" name="qindex" value="'''+qindex+'''">
-        	<input type="hidden" name="qnums" value="'''+qnumlist+'''">
-        	<input type="hidden" name="rownum" value="'''+formindex+'''"> 
-        	<input type="submit" value="Continue" /></center>
-        	<br><br><br><br>
-        	</form>
-        	</div>
-        	</body>
-        	</html>
+		nextthing='question.py'
+	else:
+		nextthing='demographics.py'       	
+	#print "main loop"
+       	print "<center><b>Question %s/%s:</b><br><br>" % (qindex, len(questions))
+       	print "<center>%s. <br><br>How does the character feel in this situation? <br> " % (question)
+       	print '''
+        <div id="page_content" align="center">
+        <form name="myform" action="%s" method="submit" onSubmit="return validate(myform)">
+        <div class="radioLeft" align="center">
+        '''%(nextthing)
+	def make_checkarray(emotionlist):
+		numemos=len(emotionlist)
+		#numcols=math.floor(math.sqrt(numemos))
+		if numemos<4:
+			numcols=numemos
+		else:
+			numcols=math.floor(math.sqrt(len(emolist)))
+		buckets=[[] for i in range(0,numcols)]
+		for n, emo in enumerate(emotionlist):
+			col=int(n%numcols)
+			emostring='<br><input type="radio" name="response" value="%s"><label for="%s">%s</label>' % (emo,emo,emo)
+			buckets[col].append(emostring)
+		mainprintout=[]
+		for c in buckets:
+			print '<div class="radioLeft" align="center">'
+			for e in c:
+				print e
+			print "</div>"
+	make_checkarray(emolist)
+	print '''
+	<br><br>
+	<p> If you feel that the situation is better described by another <br> word, please list alternative words here (optional). </p>
+        <input type="text" name="otherword1" >
+        <br><input type="text" name="otherword2">
+        <br><br>
+        <input type="hidden" name="subjid" value="'''+subjid+'''">
+        <input type="hidden" name="keycode" value="'''+keycode+'''">
+        <input type="hidden" name="qindex" value="'''+qindex+'''">
+        <input type="hidden" name="qnums" value="'''+qnumlist+'''"> 
+        <input type="hidden" name="rownum" value="'''+formindex+'''"> 	
+        <input type="submit" value="Continue" /></center>
+        <br><br><br><br>
+        </form>
+        </div>
+        </body>
+        </html>
         '''
-        
-        
         
         #end
 
